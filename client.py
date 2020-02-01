728ae761901934d94982f59f5d897cc796fb141b
keveniskander <59800096+keveniskander@users.noreply.github.com>
2020-01-31 22:12:40 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,104 +1,58 @@
-# Import socket module
-from socket import * 
-import sys # In order to terminate the program
+import socket
+import sys
 
-serverName = 'localhost'
-# Assign a port number
-serverPort = 1200
-
-# Bind the socket to server address and server port
-clientSocket = socket(AF_INET, SOCK_STREAM)
-
-clientSocket.connect((serverName, serverPort))
-
-print('1-connect')
-print('2-disconnect')
-print('3-POST')
-print('4-GET')
-print('5-PIN')
-print('6-UNPIN')
-print('7-exit')
-
-connectionEstablished = False
-
-input1 = input('Input: ')
-
-while input1 != '7':
-		
-	
-	if input1 == '3':
-
-		input2 = input('POST ')
-		
-		input2 = 'POST ' + input2
-		clientSocket.send(input2.encode())
-
-# 		output = clientSocket.recv(1024)
-
-		print('From server:', output.decode())
-
-
-		input1 = 7
+SERVER = "127.0.0.1"
+PORT = 8080
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER, PORT))
+client.sendall(bytes("This is from Client",'UTF-8'))
 
 
 
-	elif input1 == '4':
-		input2 = input('GET ')
-		
-		input2 = 'GET' + input2
-		clientSocket.send(input2.encode())
-		
-# 		output = clientSocket.recv(1024)
+while True:
+    
+    in_data =  client.recv(1024)
+    print("From Server :" ,in_data.decode())
+    print('1-connect')
+    print('2-disconnect')
+    print('3-POST')
+    print('4-GET')
+    print('5-PIN')
+    print('6-UNPIN')
+    print('7-exit')
+    
+    commandInput = input('Input: ')
+    
+    if commandInput == '3':
+        
+        out_data = input('POST ')
+        out_data = 'POST ' + out_data
+        client.sendall(bytes(out_data,'UTF-8'))
+        
+    
+    if commandInput =='2':
+       out_data = '2' 
+       client.sendall(bytes(out_data,'UTF-8'))
+        
+    if commandInput == '1':
+        out_data = '1'
+        SERVER = "127.0.0.1"
+        PORT = 8080
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((SERVER, PORT))
+        client.sendall(bytes("This is from Client",'UTF-8'))
 
-	elif input1 == '5':
-		input2 = input('PIN ')
-		
-		input2 = 'PIN ' + input2
-		clientSocket.send(input2.encode())
-		
-# 		output = clientSocket.recv(1024)
+    if commandInput == '4':
+        out_data = input('PIN ')
+        out_data = 'GET ' + out_data
+        client.sendall(bytes(out_data,'UTF-8'))
 
-	elif input1 == '6':
-		input2 = input('UNPIN ')
-		
-# 		output = clientSocket.recv(1024)
+    if commandInput == '5':
+        out_data = '5'
+        out_data = 'PIN ' + out_data
 
-	elif input1 == '1':
-		connectionEstablished = True
-		input2 = 'connect'
-		clientSocket.send(input2.encode())
-		
-# 		output = clientSocket.recv(1024)
-	elif input1 == '2':
-		print('...Disconnecting from server...')
-		input2 = 'disconnect'
-		clientSocket.send(input2.encode())
-		
-# 		output = clientSocket.recv(1024)
-		clientSocket.close()
-
-	elif input1 == '7':
-		clientSocket.send('EXIT')
-		print('...Exiting program...')
-		
-		input2()
-# 		output = clientSocket.recv(1024)
-# 		clientSocket.close()
-		sys.exit(0)
-		break
-	
-	input1 = input('Input: ')
-	if input1 != '7':
-		print('1-connect')
-		print('2-disconnect')
-		print('3-POST')
-		print('4-GET')
-		print('5-PIN')
-		print('6-UNPIN')
-		print('7-exit')
-		
-
-
-	output = clientSocket.recv(1024)
-print('From server:', output.decode())
-clientSocket.close()
+        
+    else:    
+        client.sendall(bytes(out_data,'UTF-8'))
+    
+client.close()
