2a9f0742f4e91ccb8bacac6e5e86b566bd1345c5
keveniskander <59800096+keveniskander@users.noreply.github.com>
2020-01-31 22:12:22 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,6 @@
-# Import socket module
-from socket import * 
-import sys # In order to terminate the program
-from builtins import *
+import socket, threading
+import sys
 
-# Create a TCP server socket
-#(AF_INET is used for IPv4 protocols)
-#(SOCK_STREAM is used for TCP)
-
-serverSocket = socket(AF_INET, SOCK_STREAM)
-
-# Assign a port number
-serverPort = 1200
-
-# Bind the socket to server address and server port
-serverSocket.bind(("", serverPort))
-
-# Listen to at most 1 connection at a time
-serverSocket.listen(5)
-
-print ('The server is ready to receive')
-
-# Server should be up and running and listening to the incoming connections
 
 class Note(object):
 	message= ""
@@ -42,138 +22,152 @@ class Note(object):
 		self.pins=pins
 
 	
-
-# def pin(x,y):
-# 	print('pinning')
-# 
-# def unpin(x,y):
-# 	print('unpinning')
-
-
-
-
-
-
+	
 # Declaring board parameters
 boardWidth = sys.argv[2]
 boardHeight = sys.argv[3]
 colours = []
 noteList = []
 
-# def checkColour(colours, colour):
-# 	count = 0
-# 	for i in range(len(colours)):
-# 		if colours[i]==colour:
-# 			count+=1
-# 	
-# 	print(count)
 
+# Fill colours array with colour values from argv
 i = 4
-while i < (len(sys.argv) - 1):
+while i < (len(sys.argv)):
+    	
 	colours.append(sys.argv[i])
+
 	i+=1
+	
+print('BOARD WIDTH' + boardWidth)
+print('BOARD HEIGHT' + boardHeight)
+
+
+class ClientThread(threading.Thread):
+	def __init__(self,clientAddress,clientsocket):
+		threading.Thread.__init__(self)
+		self.csocket = clientsocket
+		print ("New connection added: ", clientAddress)
+	def run(self):
+		print ("Connection from : ", clientAddress)
+		commandInput = 0
+		
+		
+		
+		#self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
+		msg = ''
+		while True:
+			print('TESTING 0')
+			
+			splitMsg =msg.split()
+			
+			if len(splitMsg) > 0:
+				commandInput = splitMsg[0]
+			
+			data = self.csocket.recv(2048)
+			msg = data.decode()
+			
+			if msg == '1':
+				break
+			
+			if msg=='2':
+				
+				break
+			print(splitMsg)
+			
+			if commandInput == 'POST':
+				
+				
+				
+				if len(splitMsg) >= 6:
+					
+					
+					
+					x = splitMsg[1]
+					y = splitMsg[2]
+					width = splitMsg[3]
+					height = splitMsg[4]
+					colour = splitMsg[5]
+					message = splitMsg[6:]
+					
+					s = ' '
+					joinedMessage = s.join(message)
+					
+					print('X ' + x)
+					print('Y ' + y)
+					
+					if (colour in colours) and (int(x) + int(width) < int(boardWidth)) and (int(y) + int(height) < int(boardWidth)):
+						
+						note = Note(x, y, width, height, colour, joinedMessage, 0)
+						noteList.append(note)
+						msg = 'NOTE POSTED'
+    					
+
+					else:
+    						
+						msg = 'NOTE NOT POSTED'
+
+				else:
+    				
+					msg = 'NOTE NOT POSTED'
+
+
+			if commandInput == 'GET':
+				print('GET')
+
+			if commandInput == 'PIN':
+				
+				if len(splitMsg) == 3:
+					
+					x = splitMsg[1]
+					y = splitMsg[2]
+					
+					for i in range(len(noteList)):
+						noteList[i].pins += 1
+
+					msg = 'PINNED'
+				msg = 'NOT PINNED'
+			
+			if commandInput == 'UNPIN':
+				
+				if len(splitMsg) == 3:
+					
+					x = splitMsg[1]
+					y = splitMsg[2]
+
+					for i in range(len(noteList)):
+							
+						if noteList[i].pins >0:
+								
+							noteList[i].pins -= 1
+
+			print ("from client", msg)
+			
+			
+			self.csocket.send(bytes(msg,'UTF-8'))
+			commandInput = ''
+			x = ''
+			y = ''
+			width = ''
+			height = ''
+			colour = ''
+			message = ''
+			
+
+
+		print ("Client at ", clientAddress , " disconnected...")
+		
+LOCALHOST = "127.0.0.1"
+PORT = 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((LOCALHOST, PORT))
+print("Server started")
+print("...Waiting for client request..")
+
 
 
 while True:
-	print('The server is ready to receive')
-	
-	# Set up a new connection from the client
-	connectionSocket, addr = serverSocket.accept()
-
-	
-
-	input2 = connectionSocket.recv(1024).decode()
-	print(input2)
-
-	splitInput = input2.split()
-	if len(splitInput)>0:
-		commandInput = splitInput[0]
-		
-	
-	if input2 == 'EXIT':
-		print('lmao1')
-		output = '...Sys Exit...'
-		sys.exit()
-		
-	elif commandInput == 'connect':
-		print('connecting')
-		output = 'connecting'
-		
-
-	elif commandInput == 'disconnect':
-		print('disconnect')
-		output = 'disconnecting'
-		
-
-	elif commandInput == 'POST':
-		x = splitInput[1]
-		y = splitInput[2]
-		width = splitInput[3]
-		height = splitInput[4]
-		colour = splitInput[5]
-		message = splitInput[6:]
-
-		s = ' '
-		joinedMessage = s.join(message)
-		
-		if (colour in colours):
-		
-			note = Note(x, y, width, height, colour, joinedMessage, 0)
-			noteList.append(note)
-			output = 'commandInput'
-		else:
-			output = 'ERROR: The note was not added since ' + colour + ' is not a valid colour'
-		
-
-	elif commandInput == 'GET':
-		
-		type = spitInput[1]
-		operant = splitInput[2]
-		name = splitInput[3]
-		
-		
-		print('lol')
-		
-	
-	elif commandInput == 'PIN':
-		
-		x = splitInput[1]
-		y = splitInput[2]
-		pinned = False
-		
-		for i in range(len(noteList)):
-			if noteList[i].x == x and noteList[i].y == y:
-				pinned = True
-				noteList[i].pins += 1 
-		  
-		if pinned == True:
-			output = 'PIN success'
-		else:
-			output = 'PIN failed'
-			
-		
-	
-	elif commandInput == 'UNPIN':
-		
-		x = splitInput[1]
-		y = splitInput[2]
-		unpinned = False
-		
-		for i in range(len(noteList)):
-			if noteList[i].x == x and noteList[i].y == y:
-				unpinned = True
-				noteList[i].pins -= 1
-				
-		if unpinned == True:
-			output = 'UNPIN failed'
-		else:
-			output = 'UNPIN sucess'
-		connectionSocket.send(output.encode())
-	
-	print(output)
-	connectionSocket.send(output.encode())
-	connectionSocket.close()
-	
-
-serverSocket.close()  
-sys.exit() #Terminate the program after sending the corresponding data	
+	server.listen(1)
+	clientsock, clientAddress = server.accept()
+	newthread = ClientThread(clientAddress, clientsock)
+	newthread.start()
