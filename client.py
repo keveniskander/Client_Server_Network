667b4b606c989324909a4e343e4fc1244b889b7d
keveniskander <59800096+keveniskander@users.noreply.github.com>
2020-02-03 14:40:44 -0500

    Update client.py

--- client.py
+++ client.py
@@ -27,32 +27,37 @@ while True:
         
         out_data = input('POST ')
         out_data = 'POST ' + out_data
-        client.sendall(bytes(out_data,'UTF-8'))
+        # client.sendall(bytes(out_data,'UTF-8'))
         
     
     if commandInput =='2':
        out_data = '2' 
-       client.sendall(bytes(out_data,'UTF-8'))
+    #    client.sendall(bytes(out_data,'UTF-8'))
         
     if commandInput == '1':
         out_data = '1'
-        SERVER = "127.0.0.1"
-        PORT = 8080
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((SERVER, PORT))
-        client.sendall(bytes("This is from Client",'UTF-8'))
+        
+        # client.sendall(bytes("This is from Client",'UTF-8'))
 
     if commandInput == '4':
-        out_data = input('PIN ')
+        out_data = input('GET ')
         out_data = 'GET ' + out_data
-        client.sendall(bytes(out_data,'UTF-8'))
+        # client.sendall(bytes(out_data,'UTF-8'))
 
     if commandInput == '5':
-        out_data = '5'
+        out_data = input('PIN ')
         out_data = 'PIN ' + out_data
-
-        
-    else:    
-        client.sendall(bytes(out_data,'UTF-8'))
     
+    if commandInput == '6':
+        out_data = input('UNPIN ')
+        out_data = 'UNPIN ' + out_data
+
+    client.sendall(bytes(out_data,'UTF-8'))
+    
+    # if(commandInput == '2'):
+    #     break
+
+    # self.csocket.send(bytes(msg,'UTF-8'))
+
+    out_data = ''
 client.close()
